ab15a111c6db98942ffa7e7ee73a929917aef44b
umang <jnumang1999@gmail.com>
2018-11-02 13:28:33 +0530

    made job page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class blog(models.Model):
+	title = models.CharField(max_length=200)
+	pub_date = models.DateTimeField()
+	body = models.TextField()
+	image = models.ImageField(upload_to='images/')
